dd4aa26905e2b5e3c5b6d1bfaad2c5accad325b8
AMIT KUMAR <amihem.net@gmail.com>
2026-07-21 01:09:49 +0530

    Correctly save supabaseClient.js with JWT anon key

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://nwdaryuauhziabxtlseza.supabase.co'
-const supabaseAnonKey = 'sb_publishable_qu0PTxwHp2hrAPoZimohXA_ajNkPfH6'
+const supabaseAnonKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im53ZGFyeXVhdWh6aWFieHRsc2V6YSIsInJvbGUiOiJhb24iLCJpYXQiOjE3MzODQ0MjkyODEsImV4cCI6MjEwMDAwNTI4MX0.3o2S1r5mOcGAIonCrXwgTjpvPo-7dYTHnSBD0vyE4Q'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
